7c4cd36e97a641ab4988fa5bd692700b58459876
Anthony Goddard <anthony@anthonygoddard.com>
2015-01-05 10:39:40 -0700

    ensure output_dir exists before writing there?

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require "chefspec/librarian"
 RSpec.configure do |config|
   config.log_level = :fatal
   output_dir = 'output'
+  Dir.new(output_dir)
   config.output_stream = File.open(File.join(output_dir,'chefspec.json'), 'w')
   config.formatter = 'json'
 end
