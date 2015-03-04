9cd198608a7834d51ecc2eb8b9ddcc89058c5e40
Anthony Goddard <anthony@anthonygoddard.com>
2015-03-03 22:05:51 -0700

    add additional failing resource

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,11 @@ remote_file "500 me" do
   source "http://httpstat.us/500"
 end
 
+remote_file "500 again" do
+  source "http://httpstat.us/500"
+end
+
+
 remote_file "broken" do
   source "http://localhost/this/will/surely/fail.txt"
 end
