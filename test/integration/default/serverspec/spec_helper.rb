b66b2a468e1129134a00cb3e320b1c0bde7ce3dc
Cameron Johnston <cameron@rootdown.net>
2015-05-20 09:00:49 -0600

    use /tmp instead of /tmp/output for writing serverspec result json

--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |c|
-  output_dir = '/tmp/output'
+  output_dir = '/tmp'
   Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
   c.output_stream = File.open(File.join(output_dir,'serverspec.json'), 'w')
   c.formatter = 'json'
