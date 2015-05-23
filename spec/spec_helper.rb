e8889b2deed17de481e34fad21a22c797a3eeaa7
Anthony Goddard <anthony@anthonygoddard.com>
2015-05-23 14:30:22 -0600

    remove custom config in spec helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,6 @@
 require "chefspec"
 require "chefspec/librarian"
 
-RSpec.configure do |config|
-  config.log_level = :fatal
-  output_dir = 'output'
-  Dir.new(output_dir)
-  config.output_stream = File.open(File.join(output_dir,'chefspec.json'), 'w')
-  config.formatter = 'json'
-end
+
 
 ChefSpec.define_matcher :sudo
--- test/integration/default/serverspec/spec_helper.rb
+++ test/integration/default/serverspec/spec_helper.rb
@@ -1,6 +1,3 @@
 RSpec.configure do |c|
-  output_dir = '/tmp/output'
-  Dir.mkdir(output_dir) unless Dir.exist?(output_dir)
-  c.output_stream = File.open(File.join(output_dir,'serverspec.json'), 'w')
-  c.formatter = 'json'
+  #not much here
 end
